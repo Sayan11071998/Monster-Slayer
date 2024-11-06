2fcba079587e8225540c742d91aac9d35ac1f6d2
Sayan Nandi <sayannandi11071998@gmail.com>
2024-11-07 01:22:56 +0530

    How to Make a Combo Sword Attack

--- Assets/Scripts/Enemy/Knight/Knight.cs
+++ Assets/Scripts/Enemy/Knight/Knight.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections), typeof(Damagable))]
 public class Knight : MonoBehaviour
 {
-    public float walkSpeed = 3f;
+    public float walkAcceleration = 3f;
+    public float maxSpeed = 3f;
     public float walkStopRate = 0.05f;
     public DetectionZone attackZone;
     public DetectionZone cliffDetectionZone;
@@ -101,7 +102,12 @@ public class Knight : MonoBehaviour
         if (!damagable.LockVelocity)
         {
             if (CanMove)
-                rb.velocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.velocity.y);
+            {
+                rb.velocity = new Vector2(
+                    Mathf.Clamp(rb.velocity.x + (walkAcceleration * walkDirectionVector.x * Time.fixedDeltaTime), -maxSpeed, maxSpeed),
+                    rb.velocity.y
+                );
+            }
             else
                 rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, walkStopRate), rb.velocity.y);
         }
